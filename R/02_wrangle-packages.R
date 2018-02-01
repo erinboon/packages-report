4dffc03ac79cbbe101e3702675e69f1df4321fc4
erinboon <erin.d.boon@gmail.com>
2018-02-01 15:14:56 -0600

    task 2 completed; testing

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -1,14 +1,19 @@
 ## create a data frame from data/installed-packages.csv
 ## with, e.g., readr::read_csv() or read.csv()
 
+library(tidyverse)
+ipt_small <- read_csv(here("data", "installed-packages.csv"))
+
 ## filter out packages in the default library
 ## keep variables Package and Built
 ## if you use dplyr, code like this will work:
-apt <- ipt %>%
+apt <- ipt_small %>%
+  filter(is.na(Priority)) %>%
   filter(LibPath == .libPaths()[1]) %>%
   select(Package, Built)
 
 ## write this new, smaller data frame to data/add-on-packages.csv
+write_csv(apt, here("data", "add-on-packages.csv"))
 
 ## make a frequency table of package by the version in Built
 ## if you use dplyr, code like this will work:
